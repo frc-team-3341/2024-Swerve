5a061ca0a26399b24371b51e515c3a8d49ccdbb8
Patrick McKinnon <pablomckinnon14@yahoo.com>
2025-01-30 10:47:39 -0800

    yagsl test

--- readme.md
+++ readme.md
@@ -1,63 +1,115 @@
-# 2024 Option 16 Swerve Repo
-This repo includes the swerve code used during the 2024 FRC Crescendo season
+# Yet Another Generic Swerve Library (YAGSL) Example project
 
-The rest of this readme file is copied from the 2023 Swerve IAP repo [here](https://github.com/frc-team-3341/2023-IAP-Swerve/blob/main/README.md)
+YAGSL is intended to be an easy implementation of a generic swerve drive that should work for most
+square swerve drives. The project is documented
+on [here](https://github.com/BroncBotz3481/YAGSL/wiki). The JSON documentation can also be
+found [here](docs/START.md)
 
-This code is updated to WPILib's 2024 release.
+This example is intended to be a starting place on how to use YAGSL. By no means is this intended to
+be the base of your robot project. YAGSL provides an easy way to generate a SwerveDrive which can be
+used in both TimedRobot and Command-Based Robot templates.
 
-## [LINK TO UML STRUCTURE DIAGRAM!!! (updated as of 1/11/2024)](https://github.com/frc-team-3341/2023-IAP-Swerve/blob/main/2024-Competition-Swerve/SwerveUMLDiagram_structure.png)
 
-### CAN ID Protocol
-| Module Name  | Drive Motor | Turn Motor | CANCoder |
-| ------------- | ------------- | ------------- | ------------- |
-| Front Left  | CAN #1  | CAN #2  | CAN #9  |
-| Front Right  | CAN #3  | CAN #4  | CAN #10  |
-| Back Left  | CAN #5  | CAN #6  | CAN #11  |
-| Back Right  | CAN #7  | CAN #8  | CAN #12  |
+# Overview
 
-## Testing updates
-### What has been done physically (non-exhaustive):
-- Set all proper CAN IDs
-- Verified all module motors function
-- Verified all CANCoders function
-- Verified correct motor inverts
-- Verified PIDF for drive motors (12/16/23)
-### What has been done in simulation (non-exhaustive):
-- Verified that all drive and turn motors function
-- Determined proper characterizations for all drive and turn motors
-- Verified that kinematics of swerve drive is correct with high-level controls
-- High-level control methods (Crab Drive, Swerve Teleop, Test Command that switches between modules) are functional
-- High-level autonomous (PathPlannerLib) is functional
-### TODO List (non-exhaustive):
-- Test PID for turn motors
-- Verify that turn motors are recording correct module angle
-- Verify that odometry is accurate
-- Verify that turn motors are responsive enough with regular kP control
-- Dry-run a "Crab Drive" mode to test basic driving
+### Installation
 
-## Non-exhaustive list of features
-- Supports 8-motor NEO swerve
-- Modularity with Java Interface class (inspired by FRC Team 3181) - in the future, we can create an Interface for TalonFX
-- Uses Java Suppliers and Consumers as well as Java Lambdas in order to supply them
-- Uses Java Object References for Autonomous code
-- Implements Trajectory driving with PathPlannerLib and PathPlanner's app by reading JSON file
-- Asymmetric Rate limiting, inspired by FRC Team NOMAD 6995's code
-- Integrated encoder support for driving motor only (due to low CPR of REV NEO turning encoder)
-- Ability to visualize Swerve Drive modules + robot in WPILib Glass
-- Ability to visualize Swerve Drive module states in AdvantageScope 2023 **(Deprecated in 2024)**
-- Ability to switch from keyboard control to XBox with one boolean **(not during matches, have to re-deploy)**
-- **Supports simulation of Swerve teleop, autonomous, and module testing code**
-  - **Only supports swerve simulation for now, not mechanisms!**
-- Convenient telemetry using centralized telemetry method in module class + Data Logging of SmartDashboard
-- Ability to switch between testing commands using SendableChooser:
-  - Regular Swerve Teleop command
-  - "Crab Drive" command for testing without gyro
-  - Module testing command with both PIDF and voltage testing, that can switch between modules
+Vendor URL:
 
-### "250 lines or less" 
-"250 lines or less" design philosophy - SwerveDrive subsystem is small compared to most other codebases. 
-Contains a Utility class in order to support more technical/Calculus features and make the code more compact.
-- Helps support students who are just learning AP Calculus AB or do not know it
-- Example of Calc usage: integrating angular velocity (calculated from module states, not odometry) in order to simulate gyro
-- Example of Calc usage: kinematics fix (discretize) for "curving" motion while chassis translates + rotates
-- Example of Calc usage: rate-limiting control differential (dC/dt) by certain rate for smoother control
+```
+https://broncbotz3481.github.io/YAGSL-Lib/yagsl/yagsl.json
+```
+
+[Javadocs here](https://broncbotz3481.github.io/YAGSL/)  
+[Library here](https://github.com/BroncBotz3481/YAGSL/)  
+[Code here](https://github.com/BroncBotz3481/YAGSL/tree/main/swervelib)  
+[WIKI](https://github.com/BroncBotz3481/YAGSL/wiki)  
+[Config Generation](https://broncbotz3481.github.io/YAGSL-Example/)
+
+# Create an issue if there is any errors you find!
+
+We will be actively montoring this and fix any issues when we can!
+
+## Development
+
+* Development happens here on `YAGSL-Example`. `YAGSL` and `YAGSL-Lib` are updated on a nightly
+  basis.
+
+# Support our developers!
+<a href='https://ko-fi.com/yagsl' target='_blank'><img height='35' style='border:0px;height:46px;' src='https://az743702.vo.msecnd.net/cdn/kofi3.png?v=0' border='0' alt='Buy Me a Robot at ko-fi.com'></a>
+
+### TL;DR Generate and download your configuration [here](https://broncbotz3481.github.io/YAGSL-Example/) and unzip it so that it follows structure below:
+
+```text
+deploy
+└── swerve
+    ├── controllerproperties.json
+    ├── modules
+    │   ├── backleft.json
+    │   ├── backright.json
+    │   ├── frontleft.json
+    │   ├── frontright.json
+    │   ├── physicalproperties.json
+    │   └── pidfproperties.json
+    └── swervedrive.json
+```
+
+### Then create your SwerveDrive object like this.
+
+```java
+import java.io.File;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.util.Units;
+
+
+SwerveDrive swerveDrive=new SwerveParser(new File(Filesystem.getDeployDirectory(),"swerve")).createSwerveDrive(Units.feetToMeters(14.5));
+```
+
+# Migrating Old Configuration Files
+
+1. Delete `wheelDiamter`, `gearRatio`, `encoderPulsePerRotation` from `physicalproperties.json`
+2. Add `optimalVoltage` to `physicalproperties.json`
+3. Delete `maxSpeed` and `optimalVoltage` from `swervedrive.json`
+4. **IF** a swerve module doesn't have the same drive motor or steering motor as the rest of the
+   swerve drive you **MUST** specify a `conversionFactor` for BOTH the drive and steering motor in
+   the modules configuration JSON file. IF one of the motors is the same as the rest of the swerve
+   drive and you want to use that `conversionFactor`, set the `conversionFactor` in the module JSON
+   configuration to 0.
+5. You MUST specify the maximum speed when creating a `SwerveDrive`
+   through `new SwerveParser(directory).createSwerveDrive(maximumSpeed);`
+6. IF you do not want to set `conversionFactor` in `swervedrive.json`. You can pass it into the
+   constructor as a parameter like this
+
+```java
+double DriveConversionFactor = SwerveMath.calculateMetersPerRotation(Units.inchesToMeters(WHEEL_DIAMETER), GEAR_RATIO, ENCODER_RESOLUTION);
+double SteeringConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(GEAR_RATIO, ENCODER_RESOLUTION);
+SwerveDrive swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, SteeringConversionFactor, DriveConversionFactor);
+```
+
+### Falcon Support would not have been possible without support from Team 1466 Webb Robotics!
+
+# Configuration Tips
+
+### My Robot Spins around uncontrollably during autonomous or when attempting to set the heading!
+
+* Invert the gyro scope.
+* Invert the drive motors for every module. (If front and back become reversed when turning)
+
+### Angle motors are erratic.
+
+* Invert the angle motor.
+
+### My robot is heavy.
+
+* Implement momentum velocity limitations in SwerveMath.
+
+### Ensure the IMU is centered on the robot
+
+# Maintainers
+- @thenetworkgrinch
+- @Technologyman00 
+
+# Special Thanks to Team 7900! Trial N' Terror
+Without the debugging and aid of Team 7900 the project could never be as stable or active as it is. 
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,210 +1,174 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-
-
-import frc.robot.commands.swerve.CrabDrive;
-import frc.robot.commands.swerve.SwerveTeleop;
-import frc.robot.commands.swerve.TestFourModules;
-import frc.robot.subsystems.swerve.SwerveDrive;
-import frc.robot.subsystems.swerve.SwerveModuleIO;
-import frc.robot.subsystems.swerve.SwerveModuleIOSim;
-import frc.robot.subsystems.swerve.SwerveModuleIOSparkMax;
-
-
+import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+import java.io.File;
+import swervelib.SwerveInputStream;
 
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a "declarative" paradigm, very
+ * little robot logic should actually be handled in the {@link Robot} periodic methods (other than the scheduler calls).
+ * Instead, the structure of the robot (including subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer
+{
 
-public class RobotContainer {
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  final         CommandXboxController driverXbox = new CommandXboxController(0);
+  // The robot's subsystems and commands are defined here...
+  private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+                                                                                "mySwerve"));
 
-  /*
-   * TO THE FUTURE READERS/REVIEWERS OF THIS FILE:
-   * Feel free to use any parts of this project or its entirety in a Competition
-   * FRC robot of any kind or team.
-   * This codebase is 95% Competition-ready (minus some minor cosmetic things). It
-   * is designed so that the modules are modular (meaning easy to switch).
-   * This technique enables us to simulate the swerve drivebase and develop at
-   * home to our heart's content.
-   * In the future, we can also write a SwerveModuleIOTalonFX.java as well and
-   * easily "plug" it in.
-   * 
-   * Minor warning: advanced Java syntax that this project uses:
-   * - Java Lambdas
-   * - Java Suppliers and Consumers
-   * - Java Interface Classes
-   * - Java For-Each Loops
+  /**
+   * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
    */
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                () -> driverXbox.getLeftY() * -1,
+                                                                () -> driverXbox.getLeftX() * -1)
+                                                            .withControllerRotationAxis(driverXbox::getRightX)
+                                                            .deadband(OperatorConstants.DEADBAND)
+                                                            .scaleTranslation(0.8)
+                                                            .allianceRelativeControl(true);
 
-  // ---------------------- START OF CONFIG SECTION --------------------------
+  /**
+   * Clone's the angular velocity input stream and converts it to a fieldRelative input stream.
+   */
+  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy().withControllerHeadingAxis(driverXbox::getRightX,
+                                                                                             driverXbox::getRightY)
+                                                           .headingWhile(true);
 
-  // Defines starting pose of robot
-  // TODO - Please remove this in future if developing for AprilTags
-  public final Pose2d startpose = new Pose2d(new Translation2d(0, 0), new Rotation2d());
+  /**
+   * Clone's the angular velocity input stream and converts it to a robotRelative input stream.
+   */
+  SwerveInputStream driveRobotOriented = driveAngularVelocity.copy().robotRelative(true)
+                                                             .allianceRelativeControl(false);
 
-  // ---------------------- END OF CONFIG SECTION --------------------------
+  SwerveInputStream driveAngularVelocityKeyboard = SwerveInputStream.of(drivebase.getSwerveDrive(),
+                                                                        () -> -driverXbox.getLeftY(),
+                                                                        () -> -driverXbox.getLeftX())
+                                                                    .withControllerRotationAxis(() -> driverXbox.getRawAxis(
+                                                                        2))
+                                                                    .deadband(OperatorConstants.DEADBAND)
+                                                                    .scaleTranslation(0.8)
+                                                                    .allianceRelativeControl(true);
+  // Derive the heading axis with math!
+  SwerveInputStream driveDirectAngleKeyboard     = driveAngularVelocityKeyboard.copy()
+                                                                               .withControllerHeadingAxis(() ->
+                                                                                                              Math.sin(
+                                                                                                                  driverXbox.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2),
+                                                                                                          () ->
+                                                                                                              Math.cos(
+                                                                                                                  driverXbox.getRawAxis(
+                                                                                                                      2) *
+                                                                                                                  Math.PI) *
+                                                                                                              (Math.PI *
+                                                                                                               2))
+                                                                               .headingWhile(true);
 
-  // Xbox + an additional one for PC use
-  private final Joystick drivingXbox = new Joystick(0);
-  private final Joystick simulationJoy = new Joystick(1);
-
-  // Chooser for testing teleop commands
-  private final SendableChooser<Command> teleopCommandChooser = new SendableChooser<>();
-  
-
-  // Define axises for using joystick
-  private final int translationAxis = XboxController.Axis.kLeftY.value; // Axis ID: 1
-  private final int strafeAxis = XboxController.Axis.kLeftX.value; // Axis ID: 0
-  private final int rotationAxis = XboxController.Axis.kRightX.value; // Axis ID: 4
-
-  // Creates array of swerve modules for use in SwerveDrive object - null in
-  // context of code
-  SwerveModuleIO[] swerveMods = new SwerveModuleIO[4];
-  // Empty SwerveDrive object
-  private SwerveDrive swerve;
-  // Empty testing commands (not used if not needed)
-  private TestFourModules allFour;
-  // Empty Auto object
-  // Empty SwerveTeleop object
-  private SwerveTeleop teleop;
-  // Empty CrabDrive object
-  private CrabDrive crabDrive;
-
-
-  // Field centric toggle - true for field centric, false for robot centric
-  private boolean fieldCentricToggle = true;
-
-
-  public RobotContainer() {
-
-    // Construct swerve subsystem with appropriate modules - DO NOT REMOVE THIS
-    this.constructSwerve();
-
-    // Create swerve commands - DO NOT REMOVE THIS
-    this.createSwerveCommands();
-
-    
-    // Construct all other things
-    this.configureBindings();
-
-
-  }
-
-  private void constructSwerve() {
-    if (Constants.currentRobot.dataLogEnabled) {
-      // Data logging works on both real + simulated robot with all DriverStation
-      // outputs!
-      DataLogManager.start();
-      DriverStation.startDataLog(DataLogManager.getLog(), false);
-      SmartDashboard.putString("Data Log Folder: ", DataLogManager.getLogDir());
-    }
-
-    // Initialize SwerveDrive object with modules
-    if (Constants.isSim) {
-      // Construct swerve modules with simulated motors
-      for (int i = 0; i < swerveMods.length; i++) {
-        swerveMods[i] = new SwerveModuleIOSim(i);
-      }
-
-    } else {
-      // Construct swerve modules with real motors
-      for (int i = 0; i < swerveMods.length; i++) {
-        swerveMods[i] = new SwerveModuleIOSparkMax(i, Constants.currentRobot.moduleCANIDs[i][0],
-            Constants.currentRobot.moduleCANIDs[i][1], Constants.currentRobot.moduleCANIDs[i][2],
-            Constants.currentRobot.moduleAngleOffsets[i], Constants.SwerveConstants.moduleInverts[i]);
-      }
-
-    }
-
-    this.swerve = new SwerveDrive(startpose, this.swerveMods[0], this.swerveMods[1], this.swerveMods[2],
-        this.swerveMods[3]);
-
-  }
-
-  private void createSwerveCommands() {
-
-    if (Constants.currentRobot.xboxEnabled) {
-      // Supply teleop command with joystick methods - USES LAMBDAS
-      teleop = new SwerveTeleop(this.swerve, () -> {
-        return -this.drivingXbox.getRawAxis(translationAxis);
-      }, () -> {
-        return -this.drivingXbox.getRawAxis(strafeAxis);
-      }, () -> {
-        return -this.drivingXbox.getRawAxis(rotationAxis);
-      }, () -> {
-        return this.drivingXbox.getRawAxis(XboxController.Axis.kRightTrigger.value);
-      }, () -> {
-
-        // Toggles between field centric (true) and robot centric (false)
-        if (this.drivingXbox.getRawButtonPressed(XboxController.Button.kX.value)) {
-          fieldCentricToggle = !fieldCentricToggle;
-
-          SmartDashboard.putBoolean("isFieldCentric", fieldCentricToggle);
-        }
-
-        return fieldCentricToggle;
-      }, Constants.currentRobot.allianceEnabled);
-
-    } else if (!Constants.currentRobot.xboxEnabled) {
-      // Supply teleop command with joystick methods - USES LAMBDAS
-      teleop = new SwerveTeleop(this.swerve, () -> {
-        return -this.drivingXbox.getX();
-      }, () -> {
-        return -this.drivingXbox.getY();
-      }, () -> {
-        return -this.simulationJoy.getRawAxis(0);
-      }, () -> {
-        return 0.0;
-      }, () -> {
-
-        // Toggles between field centric (true) and robot centric (false)
-        if (this.drivingXbox.getRawButtonPressed(1)) {
-          fieldCentricToggle = !fieldCentricToggle;
-        }
-
-        return fieldCentricToggle;
-      }, Constants.currentRobot.allianceEnabled);
-
-    }
-
-    crabDrive = new CrabDrive(this.swerve, () -> {
-      return -this.drivingXbox.getX();
-    }, () -> {
-      return -this.drivingXbox.getY();
-    });
-
-    allFour = new TestFourModules(swerve, drivingXbox);
-    
-    teleopCommandChooser.addOption("Regular Teleop", teleop);
-    teleopCommandChooser.addOption("Crab Teleop", crabDrive);
-    teleopCommandChooser.addOption("Module Test Command", allFour);
-    teleopCommandChooser.setDefaultOption("Regular Teleop", teleop);
-   
-    // autoPaths = new InitializeAutoPaths(swerve, shooter);
-  
-    SmartDashboard.putData(teleopCommandChooser);
-  }
-
-  private void configureBindings() {
-  }
-
-  public Command getAutonomousCommand() {
-    return null;
-    
-  }
-
-
-  public void initCommandInTeleop() {
-    swerve.setDefaultCommand(teleopCommandChooser.getSelected());
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer()
+  {
+    // Configure the trigger bindings
+    configureBindings();
+    DriverStation.silenceJoystickConnectionWarning(true);
+    NamedCommands.registerCommand("test", Commands.print("I EXIST"));
   }
 
   /**
-   * Gets Robot.isReal() from RobotContainer (slow when calling every loop)
-   * 
-   * @return If simulated or not
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary predicate, or via the
+   * named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4}
+   * controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
    */
+  private void configureBindings()
+  {
+
+    Command driveFieldOrientedDirectAngle      = drivebase.driveFieldOriented(driveDirectAngle);
+    Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+    Command driveRobotOrientedAngularVelocity  = drivebase.driveFieldOriented(driveRobotOriented);
+    Command driveSetpointGen = drivebase.driveWithSetpointGeneratorFieldRelative(
+        driveDirectAngle);
+    Command driveFieldOrientedDirectAngleKeyboard      = drivebase.driveFieldOriented(driveDirectAngleKeyboard);
+    Command driveFieldOrientedAnglularVelocityKeyboard = drivebase.driveFieldOriented(driveAngularVelocityKeyboard);
+    Command driveSetpointGenKeyboard = drivebase.driveWithSetpointGeneratorFieldRelative(
+        driveDirectAngleKeyboard);
+
+    if (RobotBase.isSimulation())
+    {
+      //drivebase.setDefaultCommand(driveFieldOrientedDirectAngleKeyboard);
+      drivebase.setDefaultCommand(driveRobotOrientedAngularVelocity);
+    } else
+    {
+      drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+    }
+
+    if (Robot.isSimulation())
+    {
+      driverXbox.start().onTrue(Commands.runOnce(() -> drivebase.resetOdometry(new Pose2d(3, 3, new Rotation2d()))));
+      driverXbox.button(1).whileTrue(drivebase.sysIdDriveMotorCommand());
+
+    }
+    if (DriverStation.isTest())
+    {
+      drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity); // Overrides drive command above!
+
+      driverXbox.x().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+      driverXbox.y().whileTrue(drivebase.driveToDistanceCommand(1.0, 0.2));
+      driverXbox.start().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      driverXbox.back().whileTrue(drivebase.centerModulesCommand());
+      driverXbox.leftBumper().onTrue(Commands.none());
+      driverXbox.rightBumper().onTrue(Commands.none());
+    } else
+    {
+      driverXbox.a().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      driverXbox.x().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
+      driverXbox.b().whileTrue(
+          drivebase.driveToPose(
+              new Pose2d(new Translation2d(6, 4), Rotation2d.fromDegrees(0)))
+                              );
+      driverXbox.start().whileTrue(Commands.none());
+      driverXbox.back().whileTrue(Commands.none());
+      driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
+      driverXbox.rightBumper().onTrue(Commands.none());
+    }
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand()
+  {
+    // An example command will be run in autonomous
+    return drivebase.getAutonomousCommand("New Auto");
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
 }
